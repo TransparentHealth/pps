2ede99d9d1411cce8a436a5571c296930b672693
Zachary Schreiber <zachgives@gmail.com>
2017-03-14 14:23:38 -0400

    JSON display

--- src/js/controllers/home.controller.js
+++ src/js/controllers/home.controller.js
@@ -1,8 +1,9 @@
 angular.module('mpn-generator').controller('homeCtrl', function($state, $scope, JSONService) {
 
-  this.clearData = function() {
-    JSONService.mpnData = null;
+  this.protected = JSONService.getData().hipaa.protected || undefined;
+  this.noticeUrl = JSONService.getData().hipaa.noticeUrl || undefined;
 
-  }
-
+  this.updateData = function(name, newData) {
+    JSONService.updateData(name, newData);
+  };
 });
